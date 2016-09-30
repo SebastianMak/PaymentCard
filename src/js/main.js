d3b98314a77fe9f1360605700d2c6bcf983ac549
Sebastian Mak <sebastianmak@NOCs-iMac-3.local>
2016-09-30 19:12:23 +0300

    2nd commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
     $('select').material_select();
-  });
+});
+
+$("form").submit(function(event) {
+    // Validate 'first name' field
+    var $firstName = $('[name="first_name"]');
+    if ($firstName.val() === "") {
+        $firstName.addClass('invalid');
+        $firstName
+          .siblings('.validation-messages')
+          .find('.custom-error')
+          .text('This field is required.');
+        event.preventDefault();
+    } else {
+        $firstName.addClass('valid');
+    }
+
+
+
+
+    event.preventDefault();
+
+});
